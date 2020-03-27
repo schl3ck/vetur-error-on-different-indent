a9ffe90a74d13fc3f5764ba7b61b8f230a3cf2db
schl3ck <schleck2@gmx.at>
2020-03-27 13:05:22 +0100

    Added files

---
+++ src/Errors.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr
+          v-for="task in tasks"
+          :key="task.id"
+          @select="shouldError(task)"
+          @other="errorsToo(42)"
+          @something="noError">
+          <td>some content</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "TaskList",
+  data: function() {
+    return {
+      tasks: []
+    };
+  },
+  methods: {
+    shouldError(task) {},
+    errorsToo(id) {},
+    noError() {}
+  }
+};
+</script>
